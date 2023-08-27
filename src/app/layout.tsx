2c7b10e3c4328c67c2a1d1dcd1235b8ed39cf38b
Vincent <thisoneis4business@gmail.com>
2023-08-28 01:06:27 +1200

    feat: metaicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,20 @@ const manrope = Manrope({ subsets: ["latin"] });
 export const metadata = meta({
   title: "Relax",
   description: "Relax, we'll handle it for you",
+  icons: {
+    icon: [
+      "/favicon.ico",
+      { sizes: "16x16", url: "/favicon-16x16.png" },
+      { sizes: "32x32", url: "/favicon-32x32.png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  viewport: {
+    initialScale: 1,
+    width: "device-width",
+    viewportFit: "cover",
+  },
+  manifest: "/site.webmanifest",
 });
 
 export default layout(({ children }) => {
