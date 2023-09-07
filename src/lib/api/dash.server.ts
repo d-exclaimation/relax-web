f5ee6df95ff9526faf8b082d2b3260137dd1a3bf
Vincent <thisoneis4business@gmail.com>
2023-09-07 21:44:29 +1200

    feat: make sure fetch is not cached more than it need to be

--- src/lib/api/dash.server.ts
+++ src/lib/api/dash.server.ts
@@ -2,13 +2,18 @@ import "server-only";
 import { Dashboard, IndiviualDashboard } from "../types";
 
 export const fetchDash = async () => {
-  const res = await fetch(`${process.env.RELAXING_URL}/dash`);
+  const res = await fetch(`${process.env.RELAXING_URL}/dash`, {
+    next: { revalidate: 10, tags: ["dash", "all"] },
+  });
   return (await res.json()) as Dashboard;
 };
 
 export const fetchIndiviualDash = async (id: string) => {
   const res = await fetch(
-    `${process.env.RELAXING_URL}/dash?id=${encodeURIComponent(id)}`
+    `${process.env.RELAXING_URL}/dash?id=${encodeURIComponent(id)}`,
+    {
+      next: { revalidate: 10, tags: ["dash", id] },
+    }
   );
   return (await res.json()) as IndiviualDashboard;
 };
